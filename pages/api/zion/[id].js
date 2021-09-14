1498c46101b935a50f858c6c7823199b0093877f
Gabriel Claudino Marinho <gabriel.cmunivos@protonmail.com>
2021-09-14 16:18:36 -0300

    first version of api created

---
+++ pages/api/zion/[id].js
@@ -0,0 +1,189 @@
+
+export const arr = [{
+
+  name: 'Neo - the one' ,
+  description: 'Neo tem dúvidas se ele realmente é O Escolhido quando ele conhece o Oráculo, que se apresente como uma senhora velha. Ela afirma a Neo que ele tem "o dom", mas que parece que ele está esperando por algo. Nessa hora, ela faz uma previsão: Em breve Neo precisará escolher entre salvar a vida de Morpheu, ou a sua própria; e, em qualquer caso, um deles irá morrer (este é o primeiro dos três caminhos que o Oráculo define para o Neo).',
+  team: 'resistance',
+  img: 'https://upload.wikimedia.org//wikipedia/en/thumb/c/c6/NeoTheMatrix.jpg/440px-NeoTheMatrix.jpg',
+  id: 1,
+
+
+
+},
+{
+
+  name: 'Trinity' ,
+  description: 'Na primeira parte da trilogia, Trinity é membro da Nabucodonozor, nave de Zion. Ela e Morpheus salvam Neo da morte. Depois ela quase morre nas mãos de Cypher, um membro da equipe, mas é salva pelo programador da nave. Logo após ajuda Neo a salvar Morpheus dos Agentes. No final revela que a Oráculo lhe dissera que ela iria se apaixonar pelo Escolhido ("The One"), e revive Neo, o ajudando em sua batalha contra o Agente Smith.',
+  team: 'resistance',
+  img: 'https://upload.wikimedia.org/wikipedia/en/thumb/7/7a/MatrixTrinity.jpg/220px-MatrixTrinity.jpg',
+  id: 2,
+},
+{
+
+  name: 'Morpheus' ,
+  description: "Morpheus is a Zion operative who serves in the city's defense force against attack from the Machines. His hovercraft, the Nebuchadnezzar, has far less armament than its sister craft. As with all hovercraft, the Neb is equipped with an EMP weapon to disable any sentinels that they encounter.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/9/9b/Morpheus1.jpg/revision/latest/scale-to-width-down/266?cb=20090501203241',
+  id: 3,
+},
+{
+
+  name: 'Cypher' ,
+  description: "Cypher[1] (born Reagan) was a redpill assigned to the Zion hovercraft Nebuchadnezzar under the command of Morpheus. He was also the one who betrayed the crew of the Nebuchadnezzar to aid Agent Smith to help him achieve information on the Zion mainframe.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/b/be/Cypher1.jpg/revision/latest/scale-to-width-down/323?cb=20090501213356',
+  id: 4,
+},
+{
+
+  name: 'Tank' ,
+  description: "Tank was the operator onboard the Nebuchadnezzar. He was born naturally in Zion, brother to Dozer and Zee, and died young, sometime between his final appearance in The Matrix and the start of The Matrix Reloaded, of unspecified causes. He is succeeded in his shipboard duties by Link.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/1/1e/Tank1.jpg/revision/latest/scale-to-width-down/258?cb=20090501221445',
+  id: 5,
+},
+{
+
+  name: 'The Oracle' ,
+  description: "The Oracle is a major supporting character in The Matrix franchise. She is a program with such insight into human psychology that she has clairvoyant abilities. She was initially created to interpret aspects of the human psyche and was responsible for the key principles behind the third current version of the Matrix. After having served her purpose, she went into Exile and aided the Resistance in the Machine War; she ultimately is responsible for ending the war through her manipulation of key members within the Resistance, including Neo.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/c/c9/The_Oracle.jpg/revision/latest/scale-to-width-down/350?cb=20130119085243',
+  id: 6,
+},
+{
+
+  name: 'Apoc' ,
+  description: "Apoc is a crew member on the Zion hovercraft Nebuchadnezzar.Apoc serves in various roles, from running the tracing equipment for ejecting new redpills from the Matrix, to being the main driver for the group. He wrote the Four Horsemen virus. He is frequently partnered with Switch.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/0/0f/Apoc.jpg/revision/latest/scale-to-width-down/350?cb=20070715005931',
+  id: 7,
+},
+{
+
+  name: 'Switch' ,
+  description: "Switch was a crewmember on the Zion hovercraft Nebuchadnezzar who, along with Apoc, fell victim to Cypher. They were portrayed by Belinda McClory. In the original script of The Matrix film, Switch changed genders upon entering the Matrix, which coined their name, Switch. In the real world, Switch would be male, and in the Matrix, Switch would be female - a very clever and critical point of the 'residual self-image' idea. When actress Belinda McClory auditioned for the role, she was going for only half the role - Switch's Matrix form.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/d/d4/Switch1.jpg/revision/latest/scale-to-width-down/258?cb=20090501221825',
+  id: 8,
+},
+{
+
+  name: 'Dozer' ,
+  description: "Dozer was a crew member of the Nebuchadnezzar who appeared to do double duty as its pilot and in a health and wellness capacity. He was born naturally in Zion, brother of Tank and Zee[1], and died aboard the ship[2]. He was survived by his widow, Cas, and their two children. He was played by Anthony Ray Parker.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/8/8b/Dozer.jpg/revision/latest/scale-to-width-down/230?cb=20090501222051',
+  id: 9,
+},
+{
+
+  name: 'Mouse' ,
+  description: "Mouse appears to be the youngest and most diminutive crew member on the Zion hovercraft Nebuchadnezzar. He is a pod-born zionite and is later killed by S.W.A.T. gunfire within the Matrix. He is played by the Australian actor, Matt Doran.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/e/e5/Mouse1.jpg/revision/latest/scale-to-width-down/248?cb=20090501222321',
+  id: 10,
+},
+{
+
+  name: 'Agent Smith' ,
+  description: "Agent Smith (later simply  -Smith-) was an Agent of the Matrix and the main antagonist of the series. After being initially destroyed by Neo, he became an Exile program and manifested as a computer virus; characterised by his uncanny ability to copy himself over the minds of bluepills, redpills and programs alike.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/4/4d/Agent-smith-the-matrix-movie-hd-wallpaper-2880x1800-4710.png/revision/latest/scale-to-width-down/320?cb=20140504013834',
+  id: 11,
+},
+{
+
+  name: 'Agent Jones' ,
+  description: "Agent Jones was an Agent who was a cohort of Agents Brown and Smith.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/7/72/The_Matrix_Dodge_this_%28HD%29_1756.jpg/revision/latest/scale-to-width-down/350?cb=20110811032208',
+  id: 12,
+},
+{
+
+  name: 'Agent Brown' ,
+  description: "Agent Brown was an Agent, working together with Agents Smith and Jones.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/1/1c/Agent_Brown.gif/revision/latest/scale-to-width-down/320?cb=20110811025959',
+  id: 13,
+},
+{
+
+  name: 'Agent Thompson' ,
+  description: "Agent Thompson was an Agent who was a cohort of Johnson and Jackson.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/2/21/Agent_Thompson.jpg/revision/latest/scale-to-width-down/350?cb=20110813001835',
+  id: 14,
+},
+{
+
+  name: 'The Architect' ,
+  description: "The Architect is a highly specialized, humorless program of the Machine world as well as the creator of the Matrix. As the chief administrator of the system, he is possibly a collective manifestation, or at the very least a virtual representation of the entire Machine mainframe.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/b/b5/1.gif/revision/latest/scale-to-width-down/350?cb=20091017235714',
+  id: 15,
+},
+{
+
+  name: 'The Merovingian  ' ,
+  description: "The Merovingian (sometimes called The Frenchman) is an old, powerful program that resides within the Matrix. Self-described as a -trafficker of information-, the Merovingian behaves much as a leader of a powerfully organized crime syndicate. He and his wife Persephone operate a smuggling ring providing a haven for exiled programs in the Matrix. In the films, he is portrayed by Lambert Wilson.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/b/b5/1.gif/revision/latest/scale-to-width-down/350?cb=20091017235714',
+  id: 16,
+},
+{
+
+  name: 'The Twins' ,
+  description: "The Twins are two of the Merovingian's henchmen in The Matrix film trilogy, originating from the machines' second attempt at creating a viable matrix. It is unknown why the Twins became Exiles but they are apparently very infamous with the Agents, who waste no time trying to attack them during the freeway chase.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/5/57/Twins2.jpg/revision/latest/scale-to-width-down/294?cb=20090501210551',
+  id: 17,
+},
+{
+
+  name: 'Persephone' ,
+  description: "Persephone is the wife of The Merovingian and also the one who betrayed him to aid the Resistance.",
+  team: 'programs',
+  img: 'https://static.wikia.nocookie.net/matrix/images/9/9e/Persephone_2.jpg/revision/latest/scale-to-width-down/301?cb=20130119083531',
+  id: 18,
+},
+{
+
+  name: 'Niobe' ,
+  description: "Niobe is a member of the Resistance and the captain of both the Logos and the Logos II. She served on the Logos with her first mate Ghost and operator Sparks.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/4/46/Niobe1.jpg/revision/latest/scale-to-width-down/272?cb=20090501205618',
+  id: 19,
+},
+{
+
+  name: 'Kid' ,
+  description: "Kid (born Michael Karl Popper) was a Matrix-born Zion operative, and one of the very few people to have freed themselves from the Matrix through self-substantiation. He served in the Zion Military during the Battle of Zion reloading APUs and ultimately opening the gate that allowed the Mjolnir to enter the Dock and clear it of Sentinels with an EMP blast. He idolized Neo during his life, and regarded him as humanity's savior after his death, going on to found and lead E Pluribus Neo, a hard line Zionite faction.",
+  team: 'resistance',
+  img: 'https://static.wikia.nocookie.net/matrix/images/2/23/Kid_zion.jpg/revision/latest/scale-to-width-down/350?cb=20090318121221',
+  id: 20,
+},
+
+]
+
+
+
+export default function handler(req, res) {
+
+  const id = JSON.parse( req.query.id);
+  const arrFinder = arr.filter(obj => {
+    return obj.id === id
+  })
+
+ 
+
+
+    res.status(200).json(
+      
+      
+      arrFinder
+  
+  )
+
+
+  }
+  
---
+++ pages/api/zion/index.js
@@ -0,0 +1,19 @@
+import { arr } from "./[id]"
+
+
+export default function handler(req, res) {
+
+ 
+   
+  
+  
+      res.status(200).json(
+        
+        
+        arr
+    
+    )
+  
+  
+    }
+    
